4f0672c9e8b802f11c04767dc67b1479c540f99a
yangnyeom06 <lyujin1204@gmail.com>
2025-05-21 17:58:34 +0900

    update

--- Assets/Scripts/CursorControl.cs
+++ Assets/Scripts/CursorControl.cs
@@ -4,16 +4,24 @@ using System.Collections.Generic;
 
 public class CursorControl : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = true;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
 
+        if (Input.GetKeyUp(KeyCode.LeftAlt))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
     }
 }
